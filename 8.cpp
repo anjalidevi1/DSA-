b8bbd79faf5362b5184513e578f6c35172102dca
Anjali <120460778+anjalidevi1@users.noreply.github.com>
2024-07-17 05:33:51 +0530

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float rupees;
+    cout<<"Enter rupees:";
+    cin>>rupees;
+    cout<<"Conversion from rupees to pound:"<<rupees/106.90<<endl;
+    float pound;
+    cout<<"Enter pound:";
+    cin>>pound;
+    cout<<"Conversion from pound to rupees:"<<pound*106.90<<endl;
+    return 0;
+}
