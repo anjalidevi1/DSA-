975603e4af48c37631c9b76b14303f255830df63
Anjali <120460778+anjalidevi1@users.noreply.github.com>
2024-07-19 15:03:18 +0530

    Add files via upload

---
+++ multiplication except their index.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+
+public:
+
+  vector<int> productExceptSelf(vector<int>& nums) {
+
+    int n=nums.size();
+
+    vector<int> answer(n,0);
+
+    int pro1=1,flag=0;
+
+    int c=0;
+
+    for(int i=0;i<n;i++)
+
+    {
+
+      if(nums[i]==0)
+
+      c++;
+
+      if(nums[i]!=0)
+
+      pro1=pro1*nums[i];
+
+      else
+
+      flag=1;
+
+    }
+
+    for(int i=0;i<n;i++)
+
+    {
+
+      if(c<2){
+
+      if(nums[i]!=0){
+
+        if(flag==1)
+
+          answer[i]=0;
+
+        else
+
+         answer[i]=pro1/nums[i];
+
+      }
+
+      else
+
+        if(flag==1)
+
+          answer[i]=pro1;
+
+        else
+
+          answer[i]=0;
+
+      }
+
+    }
+
+    return answer;
+
+  }
+
+};
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = solution.productExceptSelf(nums);
+
+    cout << "The product of array elements except self are: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
