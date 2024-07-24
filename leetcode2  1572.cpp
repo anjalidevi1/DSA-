811a8ac25e3cfc948ac295f2412baad34e808920
Anjali <120460778+anjalidevi1@users.noreply.github.com>
2024-07-24 15:03:47 +0530

    Add files via upload

---
+++ leetcode2  1572.cpp
@@ -0,0 +1,96 @@
+---------------
+class Solution {
+
+public:
+
+  int diagonalSum(vector<vector<int>>& mat) {
+
+    int diagonalSum=0;
+
+    int n=mat.size();
+
+    for(int i=0;i<n;i++)
+
+    {
+
+      for(int j=0;j<n;j++)
+
+      {
+
+        if(i==j){
+
+          diagonalSum+=mat[i][j];
+
+        }
+
+        else if(i==n-j-1){
+
+          diagonalSum+=mat[i][j];
+
+        }
+
+      }
+
+    }
+
+    return diagonalSum;
+
+  }
+
+};
+--------------
+class Solution {
+
+public:
+
+  int diagonalSum(vector<vector<int>>& mat) {
+
+    int sum=0;
+
+    int temp=mat.size();
+
+    for(int i=0;i<mat.size();i++){
+
+      for(int j=0;j<mat.size();j++){
+
+        if(temp%2==0){
+
+          if(i==j){
+
+            sum+=mat[i][j];
+
+          }
+
+          if(i+j==temp-1){
+
+            sum+=mat[i][j];
+
+          }
+
+        }
+
+        else{
+
+          if(i+j==temp-1){
+
+            sum+=mat[i][j];
+
+          }
+
+          if(i==j && i!=temp/2){
+
+            sum+=mat[i][j];
+
+          }
+
+        }
+
+      }
+
+    }
+
+    return sum;
+
+  }
+
+};
